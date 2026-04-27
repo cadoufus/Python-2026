d30c60f67ab0ef1ac711a0deb3703c0e06ba9fdb
cadoufus <artolivsouz@gmail.com>
2026-04-27 11:23:30 -0300

    Aula sobre "for"

---
+++ aula04-estruturas-de-repeticao/aula04b-for.py
@@ -0,0 +1,15 @@
+# for count_music in range(3):
+#     print(f"Musica {count_music}")
+#
+# for i in range(1, 12, 2):
+#     print(i)
+#
+
+# musicas = int(input("Digite a quantidade de musicas: "))
+#
+# for i in range(musicas):
+#     print(f"Musica {i}")
+
+for i in range(0, 4):
+    for j in range(0, 3):
+        print(i, "|", j)
