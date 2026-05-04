942eb7ef6d28364f86b170df2cf9d093efa1a817
cadoufus <artolivsouz@gmail.com>
2026-05-04 10:57:07 -0300

    Explicação arrays

---
+++ Aula05-strings-arrays-matrizes/aula05b-arrays.py
@@ -0,0 +1,14 @@
+lista_frutas = ["maçã", "banana", "pêssego"]
+
+print(lista_frutas[0])
+
+lista_frutas.append("uva")
+
+print(lista_frutas)
+
+for i in range(len(lista_frutas)):
+    print(lista_frutas[i])
+
+print()
+for fruta in lista_frutas:
+    print(fruta)
