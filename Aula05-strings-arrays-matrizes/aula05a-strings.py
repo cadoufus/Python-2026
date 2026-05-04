5a63651c1fbd2725bd56912efcff92fc083b8fa4
cadoufus <artolivsouz@gmail.com>
2026-05-04 10:56:52 -0300

    Explicação strings

---
+++ Aula05-strings-arrays-matrizes/aula05a-strings.py
@@ -0,0 +1,12 @@
+texto = "FIAP paulista"
+print(texto[0])
+
+tamanho = len(texto)
+print(tamanho)
+print()
+
+#for i in range(tamanho):
+#   print(f"texto[{i}] = {texto[i]}")
+
+for c in texto:
+     print(c)
